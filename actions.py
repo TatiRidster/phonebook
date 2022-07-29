dc4faad106d542222ef27fe659df93550525cb9a
po100lit <po100lit@mail.ru>
2022-07-29 17:04:00 +0600

    do it yourself

---
+++ actions.py
@@ -0,0 +1,18 @@
+def edit_person():
+    pass
+
+
+def del_person():
+    pass
+
+
+def add_person():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from art import text2art
 from import_export import *
+from actions import *
 
 
 def main():
@@ -7,6 +8,9 @@ def main():
     phonebook = read_all_data('phonebook.csv')
     data_json = create_json_data(phonebook)
     write_data(data_json)
+    add_person()  # Доработать и вставить, где добавляется элемент в справочник
+    edit_person()  # Доработать и вставить, где будет редактирование справочника
+    del_person()  # Доработать и вставить, где нао удалить элемент справочника
 
 
 if __name__ == '__main__':
