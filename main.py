710794da67120527f90f19668289c2aaf711be47
po100lit <po100lit@mail.ru>
2022-07-29 09:49:45 +0600

    add requirements.txt, welcome logo, import CSV, export JSON

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from art import text2art
+from import_export import *
+
 
 def main():
-    pass
+    print(text2art('PHONEBOOK', font='sub-zero'))  # Welcome 'LOGO'
+    phonebook = read_all_data('phonebook.csv')
+    data_json = create_json_data(phonebook)
+    write_data(data_json)
 
 
 if __name__ == '__main__':
