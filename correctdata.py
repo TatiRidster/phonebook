d69e3d2569b8bb870a41383bbedba762e8700bdd
Tatiana <tatnovid@gmail.com>
2022-07-30 21:26:38 +0300

    проверка работоспособности и изменения кода

--- correctdata.py
+++ correctdata.py
@@ -1,13 +1,13 @@
 def correct_text(text):
-    name = input(f'{text} ')
+    name = text
     while True:
         if name.isalpha():
             return name.capitalize()
         print('не корректный ввод')
-        name = input(f'{text} ')
+        name = input(f'Введите снова: ')
 
-def correct_number():
-    number = input('номер +7 код номер без пробелов -> ')
+def correct_number(user_phone):
+    number = user_phone
     while True:
         if number[0] == '+' and number[1:].isdigit() and len(number) == 12:
             return number
