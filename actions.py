2f768b589ce63826ccb518763c03bc6c587984e4
po100lit <po100lit@mail.ru>
2022-07-30 10:47:30 +0600

    добавил поиск по справочнику

--- actions.py
+++ actions.py
@@ -10,6 +10,14 @@ def add_person():
     pass
 
 
+def find_person(data_list: list):
+    search_string = input('Введите данные для поиска: ')
+    for i in data_list:
+        for j in i:
+            if search_string.lower() in j.lower():
+                print(i)
+
+
 def main():
     pass
 
